716ca17c0c48447166db22e7568e64510d2f4131
wfparrish <williparrish@gmail.com>
2025-02-22 14:23:54 -0700

    Added the new items to the ProductSector in the Table Top POS side of the application.

--- tabletoppos/FoodItem.js
+++ tabletoppos/FoodItem.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { View, Text, Image, Button, StyleSheet, Dimensions } from "react-native";
 
+// 1) Create a dictionary of title → require(...) pairs
+const images = {
+  "Burger": require("./assets/images/burger.jpg"),
+  "Fries": require("./assets/images/fries.jpg"),
+  "Shakes": require("./assets/images/shakes.jpg"),
+  "Pancakes": require("./assets/images/pancakes.jpg"),
+  "Eggs": require("./assets/images/eggs.jpg"),
+  "Bacon": require("./assets/images/bacon.jpg"),
+  "Coffee": require("./assets/images/coffee.jpg"),
+  "Steak": require("./assets/images/steak.jpg"),
+  "Potatoes": require("./assets/images/potatoes.jpg"),
+  "Veggies": require("./assets/images/veggies.jpg"),
+  "Beer": require("./assets/images/beer.jpg"),
+  "Ice Cream": require("./assets/images/icecream.jpg"),
+  "Donut": require("./assets/images/donut.jpg"),
+  "Jello": require("./assets/images/jello.jpg"),
+  "Cake": require("./assets/images/cake.jpg"),
+  "Pie": require("./assets/images/pie.jpg"),
+};
+
+// 2) Lookup function that returns the appropriate require(...) or null
 const getImage = (title) => {
-  switch (title) {
-    case "Burger":
-      return require("./assets/images/burger.jpg");
-    case "Fries":
-      return require("./assets/images/fries.jpg");
-    case "Shakes":
-      return require("./assets/images/shakes.jpg");
-    default:
-      return null;
-  }
+  return images[title] || null;
 };
 
 const FoodItem = ({ title, price, text, onAddToOrder }) => {
@@ -37,12 +49,12 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 4,
     elevation: 2,
-    width: Dimensions.get("window").width > 768 ? "30%" : "45%", // Adjust width dynamically
+    width: Dimensions.get("window").width > 768 ? "30%" : "45%",
   },
   image: {
     width: 100,
     height: 100,
-    resizeMode: "contain",
+    resizeMode: "contain", // or 'cover', etc.
     marginBottom: 5,
   },
   title: {
